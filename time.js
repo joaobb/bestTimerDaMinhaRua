b8b8d26977e079d2b88b15a531ac16a7055f61d1
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2018-09-08 23:45:18 -0300

    First commit

---
+++ time.js
@@ -0,0 +1,10 @@
+var hora = new Date().toLocaleTimeString();
+
+
+const readline = require('minutes')
+
+var horaTarget = int(hora.split()[1]) + readline
+
+
+console.log(hora);
+console.log(horaTarget);
